3ad244ae099f1aa0e457482177b02f5cd94dfee1
Will Brown <wcbrow02@gmail.com>
2024-11-23 17:50:00 -0500

    [config.c] Actually read config

--- config.c
+++ config.c
@@ -5,7 +5,10 @@
 struct zblock_config zblock_config;
 
 int zblock_config_load(struct discord *client) {
-	// TODO: actually load config
-	zblock_config.conninfo = "feeds";
+	struct ccord_szbuf_readonly conninfo = discord_config_get_field(client, (char *[2]){"zblock", "conninfo"}, 2);
+	if (asprintf(&zblock_config.conninfo, "%.*s", (int)conninfo.size, conninfo.start) < 0) {
+		return 1;
+	}
+	
 	return 0;
 }
